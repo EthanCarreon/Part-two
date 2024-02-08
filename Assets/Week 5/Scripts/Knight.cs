ebb3dcbbc806f8e8d91878cda319957f94cfb0bf
carreoet <carreoet@OA-S141-03>
2024-02-08 11:16:29 -0500

    Made knight health bar

--- Assets/Week 5/Scripts/Knight.cs
+++ Assets/Week 5/Scripts/Knight.cs
@@ -13,6 +13,8 @@ public class Knight : MonoBehaviour
     bool clickingOnSelf = false;
     public float health;
     public float maxHealth = 5;
+    bool isDead = false;
+    public HealthBar healthBar;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -22,6 +24,8 @@ public class Knight : MonoBehaviour
 
     void FixedUpdate()
     {
+        if (isDead) return;
+
         movement = destination - (Vector2)transform.position;
         if (movement.magnitude < 0.1)
         {
@@ -42,8 +46,11 @@ public class Knight : MonoBehaviour
 
     private void OnMouseDown()
     {
+        if (isDead) return;
+
         clickingOnSelf = true;
         TakeDamage(1);
+        healthBar.TakeDamage(1);
     }
 
     private void OnMouseUp()
@@ -51,16 +58,18 @@ public class Knight : MonoBehaviour
         clickingOnSelf = false;
     }
 
-    void TakeDamage(float damage)
+    public void TakeDamage(float damage)
     {
         health -= damage;
         health = Mathf.Clamp(health, 0, maxHealth);
         if (health == 0)
         {
+            isDead = true;
             animator.SetTrigger("Death");
         }
         else
         {
+            isDead = false;
             animator.SetTrigger("TakeDamage");
         }
     }
